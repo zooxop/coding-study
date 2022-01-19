c0257dfd60ca0f38679a16bdcc98cb88cf65dd6c
CheolHyun Mun <zooxop@gmail.com>
2022-01-19 18:14:09 +0900

    add Q15552.java

---
+++ src/backjoon/Q15552.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q15552 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i=0; i < T; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
