a4ffc67549c3383e56486f67589e6650e129a44d
CheolHyun Mun <zooxop@gmail.com>
2022-02-06 01:51:34 +0900

    add Q15596.java

---
+++ src/backjoon/Q15596.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+public class Q15596 {
+    public static void main(String[] args) {
+
+    }
+
+    public static long sum(int[] a){
+        long ans = 0;
+
+        for (int i : a){
+            ans += i;
+        }
+        return ans;
+    }
+}
