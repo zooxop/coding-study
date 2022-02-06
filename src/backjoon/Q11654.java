297a045585cb7599f198e9b208648a001c7f05e0
CheolHyun Mun <zooxop@gmail.com>
2022-02-06 10:21:01 +0900

    Q11654.java

---
+++ src/backjoon/Q11654.java
@@ -0,0 +1,12 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q11654 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = br.readLine().charAt(0);
+        System.out.println(input);
+    }
+}
